dae945e5cce7ac7ab137e60bef7ffd99a1f73eb4
Alex-JX <alexjx17@gmail.com>
2022-07-27 14:58:29 -0700

    Added authentication_request method to the applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
+    include JsonWebToken
 
-    
+    before_action :authentication_request
+
+    private
+    def authentication_request
+        header = request.headers['Authorization']
+        token = header.split(' ').last if header
+        decoded = jwt_decode(token)
+        @current_user = User.find(decoded[:user_id])
+    end
+
 end
